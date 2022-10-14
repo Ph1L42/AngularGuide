12e890db9a8f13b620dc6bb06249eb1f47d4761a
ADESSO\hphan <hoang-dung-philipp.phan@adesso.de>
2022-10-14 16:16:18 +0200

    Real Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { DatabindingComponent } from './databinding/databinding.component';
-import { PropertyBindingComponent } from './databinding/property-binding-component';
-import { EventBindingComponent } from './databinding/event-binding.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DatabindingComponent,
-    PropertyBindingComponent,
-    EventBindingComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
