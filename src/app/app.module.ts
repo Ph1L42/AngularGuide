77ef3db4b31d9b390cfd2f069fc8042726b87f83
ADESSO\hphan <hoang-dung-philipp.phan@adesso.de>
2022-10-17 10:20:40 +0200

    First Bootstrap Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
