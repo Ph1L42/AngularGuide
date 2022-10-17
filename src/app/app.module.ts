965c563030f3a2f6910a4e9f9721c710163b4390
ADESSO\hphan <hoang-dung-philipp.phan@adesso.de>
2022-10-17 10:58:10 +0200

    Recipe Item added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RecipeComponent } from './recipe/recipe.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.c
     RecipeComponent,
     ShoppingListComponent,
     RecipeListComponent,
-    RecipeDetailsComponent
+    RecipeDetailsComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
