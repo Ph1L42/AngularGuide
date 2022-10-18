a7e69b4a48d50d6434cb8209c610300b0f76b280
ADESSO\hphan <hoang-dung-philipp.phan@adesso.de>
2022-10-18 11:22:02 +0200

    Recipe and Ingredients Added

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,9 +10,20 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
-  recipe = new Recipe(
-    'CR7', 'SIUUUUUU', 'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltfcdeffabeec48348/60dc14140401cb0ebfac177e/cc0acdf7dc2968346cc8d86dc76b6763cbb8b8dd.jpg?format=jpg'
-  );
+  recipes : Recipe[] =[
+    new Recipe(
+    'CR7', 
+    'SIUUUUUU', 
+    'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltfcdeffabeec48348/60dc14140401cb0ebfac177e/cc0acdf7dc2968346cc8d86dc76b6763cbb8b8dd.jpg?format=jpg',
+    [new Ingredient('CL', 5), new Ingredient('SIU´s', 7)]
+    ),
+    new Recipe(
+      'Schnitzel',
+      'Das ist ein crispy cleanes Chicken Schnitzel',
+      'http://kmetro.restaurantden.com/wp-content/uploads/sites/67/2016/03/dish_1.jpg',
+      [new Ingredient('Hühnchen', 1), new Ingredient('Eier', 1), new Ingredient('Mehl', 100)]
+    ),
+  ]; 
 
   selectedRecipe: Recipe;
 
