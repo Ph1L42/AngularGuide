e31fabae31b8c3af3295a29080427cc64be79f7a
ADESSO\hphan <hoang-dung-philipp.phan@adesso.de>
2022-10-18 15:29:43 +0200

    Recipe Service Added

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,40 +1,23 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Ingredient } from 'src/app/shared/ingredient.model';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
-  recipes : Recipe[] =[
-    new Recipe(
-    'CR7', 
-    'SIUUUUUU', 
-    'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltfcdeffabeec48348/60dc14140401cb0ebfac177e/cc0acdf7dc2968346cc8d86dc76b6763cbb8b8dd.jpg?format=jpg',
-    [new Ingredient('CL', 5), new Ingredient('SIU´s', 7)]
-    ),
-    new Recipe(
-      'Schnitzel',
-      'Das ist ein crispy cleanes Chicken Schnitzel',
-      'https://i.ytimg.com/vi/vYpWgclKXWg/maxresdefault.jpg',
-      [new Ingredient('Hühnchen', 1), 
-      new Ingredient('Eier', 1), 
-      new Ingredient('Mehl', 100), 
-      new Ingredient('Fresher Boi', 1),
-      new Ingredient('Notification Gang', 1)
-    ]
-    ),
-  ]; 
-
+  recipes : Recipe[]
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(){
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onSelected(recipe: Recipe){
---
+++ src/app/recipe/recipe.service.ts
@@ -0,0 +1,28 @@
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+
+export class RecipeService{
+    private recipes: Recipe[] = [
+        new Recipe(
+        'CR7', 
+        'SIUUUUUU', 
+        'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltfcdeffabeec48348/60dc14140401cb0ebfac177e/cc0acdf7dc2968346cc8d86dc76b6763cbb8b8dd.jpg?format=jpg',
+        [new Ingredient('CL', 5), new Ingredient('SIU´s', 7)]
+        ),
+        new Recipe(
+          'Schnitzel',
+          'Das ist ein crispy cleanes Chicken Schnitzel',
+          'https://i.ytimg.com/vi/vYpWgclKXWg/maxresdefault.jpg',
+          [new Ingredient('Hühnchen', 1), 
+          new Ingredient('Eier', 1), 
+          new Ingredient('Mehl', 100), 
+          new Ingredient('Fresher Boi', 1),
+          new Ingredient('Notification Gang', 1)
+        ]
+        ),
+      ]; 
+
+    getRecipes(){
+        return this.recipes;
+    }
+}
